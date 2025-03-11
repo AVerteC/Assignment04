01af3d098556c81524ccbc2cd9229860af54b774
Alan Chen <alrichen@ucsc.edu>
2025-03-11 03:49:37 -0700

    fixed capitalization, added function to get user input on ex02

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -1,39 +1,48 @@
+function getUserChoice() {
+    let userChoice = "";
+    do {
+        userChoice = prompt("Enter rock, paper, or scissors:");
+        if (userChoice !== null) {
+            userChoice = userChoice.toLowerCase();
+        }
+    } while (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors" && userChoice !== null);
+    
+    // if userChoice is null that means they clicked the cancel button
+    return userChoice;
+}
 
 function game() {
-    computerChoice = Math.floor(Math.random() * 3);
-let userChoice =""; 
-userChoice = prompt("Enter rock, paper or scissors:");
-while (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
-    userChoice = prompt("Invalid choice. Enter rock, paper or scissors:");
-    if (userChoice !== null) {
-        userChoice = userChoice.toLowerCase();
+    let userChoice = getUserChoice();
+    if (userChoice === null) {
+        alert("User canceled input. Game ended.")
+        return;
     }
-}
 
-if (computerChoice === 0) {
-    computerChoice = 'rock';
-} else if (computerChoice === 1) {
-    computerChoice = 'paper';
-} else if (computerChoice === 2) {
-    computerChoice = 'scissors';
-}
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        computerChoice = 'rock';
+    } else if (computerChoice === 1) {
+        computerChoice = 'paper';
+    } else if (computerChoice === 2) {
+        computerChoice = 'scissors';
+    }
 
-// Calculate outcome
-if (userChoice === computerChoice) {
-    alert("It's a tie!");
-}
-else if (userChoice === 'rock' && computerChoice === 'scissors') {
-    alert("You win! Rock beats scissors.");
-}
-else if (userChoice === 'paper' && computerChoice === 'rock') {
-    alert("You win! Paper beats rock.");
-}
-else if (userChoice === 'scissors' && computerChoice === 'paper') {
-    alert("You win! Scissors beat paper.");
-}
-else {
-    alert("You lose! " + computerChoice + " beats " + userChoice + ".");
-}
+    // Calculate outcome
+    if (userChoice === computerChoice) {
+        alert("It's a tie!");
+    }
+    else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        alert("You win! Rock beats scissors.");
+    }
+    else if (userChoice === 'paper' && computerChoice === 'rock') {
+        alert("You win! Paper beats rock.");
+    }
+    else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        alert("You win! Scissors beat paper.");
+    }
+    else {
+        alert(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${userChoice}.`);
+    }
 }
 
 let playAgain = "y";
--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,9 +1,29 @@
 // ADD A FUNCTION CALLED CALCULATE
-
+function calculate(x, y, op) {
+    if (operation = "+") {
+        return x + y;
+    }
+    else if (operation = "-") {
+        return x - y;
+    }
+    else if (operation = "*") {
+        return x * y;
+    }
+    else if (operation = "/") {
+        return x / y;
+    }
+}
 // COLLECT FIRST NUMBER FROM USER
+while (isNaN(a));
+
+let numOne = prompt("Enter first number:");
+let a = parseFloat(numOne);
 
 // COLLECT SECOND NUMBER FROM USER
+let numTwo = prompt("Enter second number:");
+let b = parseFloat(numTwo);
 
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+let 
 
 // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
