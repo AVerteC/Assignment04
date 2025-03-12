f22ebec764d2c565e31d23ad8410424617b29d1f
Alan Chen <alrichen@ucsc.edu>
2025-03-12 16:22:47 -0700

    finished ex03

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -16,9 +16,11 @@ function getUserInputNumber(message) {
     let userInput;
     do {
         userInput = prompt(`${message}`);
-    } while (isNaN(userInput) || userInput === null);
-    console.log(userInput);
-    return userInput;
+        if (userInput !== null) {
+            userInput = userInput.trim(); // trim whitespace
+        }
+    } while (userInput === "" || isNaN(userInput) || userInput === null);
+    return parseFloat(userInput);
 }
 
 function getOperation() {
@@ -37,18 +39,19 @@ function getOperation() {
 function mainLogic() {
     // COLLECT FIRST NUMBER FROM USER
     let numOne = getUserInputNumber("Enter first number:");
-    let a = parseFloat(numOne);
 
     // COLLECT SECOND NUMBER FROM USER
     let numTwo = getUserInputNumber("Enter second number:");
-    let b = parseFloat(numTwo);
 
     // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
     let operation = getOperation();
 
     // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
-    let answer = calculate(a, b, operation);
-    alert(`${a} ${operation} ${b} = ${answer}`);
+    let answer = calculate(numOne, numTwo, operation);
+    if (isFinite(answer) === false) {
+        answer = "Can't divide by 0";
+    }
+    alert(`${numOne} ${operation} ${numTwo} = ${answer}`);
 }
 
 let goAgain = "y";
